8a7407157136bb9884ab552778a1de9044ef6771
Lindsey <lindsey_mccluskey@yahoo.ca>
2024-11-02 19:46:29 -0700

    done

--- api/shippers/shippers-middleware.js
+++ api/shippers/shippers-middleware.js
@@ -1,12 +1,23 @@
+const db = require('../../data/db-config')
+
 module.exports = {
   checkId,
   checkPayload,
 }
 
-function checkId(req, res, next) {
-  next()
+async function checkId(req, res, next) {
+  const shipper = await db('shippers').where('shipperid', req.params.id).first()
+  if(!shipper){
+    next({ status: 404, message: "That ID does not exist"})
+  } else {
+    next()
+  }
 }
 
 function checkPayload(req, res, next) {
-  next()
+  if(!req.body.phone || !req.body.ShipperName) {
+    next({ status: 422, message: "Phone and shipper name are required"})
+  } else {
+    next()
+  }
 }
